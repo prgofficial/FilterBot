d5562b3d364610ff51f066ba9d6f2e2db7209e4d
DarK Angel <60337323+DarkAngelTG@users.noreply.github.com>
2020-05-21 10:18:16 +0530

    Update disable.py
    Removed Command Disabling

--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -146,15 +146,9 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat_id)
 
 
-    __mod_name__ = "♿COMMAND DISABLING♿"
+    __mod_name__ = ""
 
     __help__ = """
- - /cmds: check the current status of disabled commands
-
-*Admin only:*
- - /enable <cmd name>: enable that command
- - /disable <cmd name>: disable that command
- - /listcmds: list all possible toggleable commands
     """
 
     DISABLE_HANDLER = CommandHandler("disable", disable, pass_args=True, filters=Filters.group)
